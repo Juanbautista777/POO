851ae49073308580c1b485e845fe024d474878d2
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-08-06 21:00:03 +0000

    Creacion de la variable X

--- Clase 1/HolaMundo.java
+++ Clase 1/HolaMundo.java
@@ -4,6 +4,11 @@ public static void main(String[] args){
 
     System.out.println("mi primer hola mundo en JAVA !!");
 
+    //CREACION DE UNA VARIABLE X DE TIPO ENTERO 
+    int x = 2;
+
+
+
 }
 
 }
