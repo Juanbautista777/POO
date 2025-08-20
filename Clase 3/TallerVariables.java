7076071ea08f6cde7c326c29187651f6c80ecdb6
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-08-20 19:40:57 +0000

    se crean las variables

--- Clase 3/TallerVariables.java
+++ Clase 3/TallerVariables.java
@@ -1,5 +1,47 @@
 package Clase 3;
 
 public class TallerVariables {
-    
+
+    public static void main (String[] args){
+
+double d1 = 42.0;
+
+double d2 = 58.5;
+
+double d3 = 37.2;
+
+double t1= 0.9;
+
+double t2= 1.4;
+
+double t3= 0.8;
+
+double l1= 5.1;
+
+double l2= 6.9;
+
+double l3= 4.3;
+
+double preciolitro = 1.35;
+
+double masaCargaKg = 1200;
+
+double largo = 2.0;
+
+double ancho = 1.2;
+
+double alto= 1.1;
+
+double p1 = 2.5;
+
+double p2 = 3.0;
+
+double p3 = 2.0;
+
+double deprecporKm = 0.08;
+
+
+
+}
+
 }
