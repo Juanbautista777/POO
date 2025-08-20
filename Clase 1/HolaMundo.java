6ee71d8eb76c58155bbf13060267391c61ff73b2
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-08-20 20:15:28 +0000

    correcion comentarios

--- Clase 1/HolaMundo.java
+++ Clase 1/HolaMundo.java
@@ -4,8 +4,8 @@ public static void main(String[] args){
 
     System.out.println("mi primer hola mundo en JAVA !!");
 
-    //CREACION DE UNA VARIABLE X DE TIPO ENTERO 
-    int x = 2;
+    /* CREACION DE UNA VARIABLE X DE TIPO ENTERO 
+    int x = 2; */
 
 
 
--- Clase 3/TallerVariables.java
+++ Clase 3/TallerVariables.java
@@ -67,6 +67,65 @@ System.out.println("\"1) Datos base de el viaje declarados: ");
 
 System.out.println("d1:" + d1 );
 
+System.out.println("d2:" + d2 );
+
+System.out.println("d3:" + d3 );
+
+System.out.println("t1:" + t1 );
+
+System.out.println("t2:" + t2 );
+
+System.out.println("t3:" + t3 );
+
+System.out.println("l1:" + l1 );
+
+System.out.println("l2:" + l2 );
+
+System.out.println("l3:" + l3 );
+
+System.out.println("d1:" + d1 );
+
+System.out.println("precioxlitro:" + preciolitro );
+
+System.out.println("masaCargaKg:" + masaCargaKg );
+
+System.out.println("largo:" + largo );
+
+System.out.println("ancho:" + ancho);
+
+System.out.println("alto:" + alto );
+
+System.out.println("p1:" + p1 );
+
+System.out.println("p2:" + p2 );
+
+System.out.println("p3:" + p3 );
+
+System.out.println("deprecporKm:" + deprecporKm );
+
+System.out.println("volcamionm3:" + volCamionm3 );
+
+System.out.println("fco2:" + fco2 );
+
+System.out.println("galon x litro :" + galporlitro );
+
+System.out.println("Velocidad minima:" + vmin );
+
+System.out.println("Velocidad maxima:" + vmax );
+
+System.out.println("Va:" + va );
+
+System.out.println("vb:" + vb );
+
+System.out.println("ca:" + ca );
+
+System.out.println("cb:" + cb );
+
+System.out.println("a:" + a );
+
+System.out.println("b:" + b );
+
+System.out.println("c:" + c );
 }
 
 }
