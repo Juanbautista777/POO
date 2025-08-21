84e463062b134dc64ddd740aeb0746b07615d33a
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-08-21 03:36:23 +0000

    practica con ejercicios mas complejos

---
+++ EJERCICIOS DE PRACTICA /practica.java
@@ -0,0 +1,22 @@
+public class practica {
+    public static void main (String[] args){
+double R1 = 2200;
+
+double R2 = 10000; 
+
+double V = 10.0;
+
+double VBB = R1 / (R2 + R1);
+
+System.out.println("La  resistencia # 1es de:" + R1 );
+
+System.out.println("La resistencia es # 2de:" + R2 );
+
+System.out.println("El voltaje es de:" + V );
+
+System.out.println("La Carga de la tension de salida es de:" + VBB );
+
+}
+
+
+}
