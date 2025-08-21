7264545ddea970eea742d0573a9869232a50f55f
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-08-21 03:57:36 +0000

    Practica ejercicio 2

--- EJERCICIOS DE PRACTICA /practica.java
+++ EJERCICIOS DE PRACTICA /practica.java
@@ -1,5 +1,6 @@
 public class practica {
     public static void main (String[] args){
+
 double R1 = 2200;
 
 double R2 = 10000; 
@@ -8,13 +9,43 @@ double V = 10.0;
 
 double VBB = R1 / (R2 + R1);
 
-System.out.println("La  resistencia # 1es de:" + R1 );
+System.out.println("******** PRACTICA CON EJERCICIOS COMPLEJOS ********");
 
-System.out.println("La resistencia es # 2de:" + R2 );
+System.out.println("******** EJERCICIO 1: ********");
+
+System.out.println("La  resistencia #1 es de:" + R1 );
+
+System.out.println("La resistencia es #2 de:" + R2 );
 
 System.out.println("El voltaje es de:" + V );
 
 System.out.println("La Carga de la tension de salida es de:" + VBB );
+
+System.out.println("******** EJERCICIO 2: ********");
+
+double V1 = 1.8;
+
+double V2 = 0.7;
+
+double KO1 = 1000;
+
+double KO2 = 1800;
+
+double Ie = (V1 - V2) / (KO1 + (KO2 / 200.0)) ;
+
+System.out.println("El voltaje #1 es de: "+ V1);
+
+System.out.println("El voltaje #2 es de: "+ V2);
+
+System.out.println("La Resistencia #1 es de: "+ KO1);
+
+System.out.println("La Resistencia #2 es de: "+ KO2);
+
+System.out.println("La Corriente del emisor es de: "+ Ie * 1000);
+
+
+
+
 
 }
 
