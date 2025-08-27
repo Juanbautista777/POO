cec192cdc278a0c1bb6fca670aa80ad0aafb19e1
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-08-27 21:37:09 +0000

    todos los ejercicios del 4.1 resueltos

--- CLase 4/PracticaDeCondicionales.java
+++ CLase 4/PracticaDeCondicionales.java
@@ -14,12 +14,55 @@ public class PracticaDeCondicionales {
             }
 
 
+        /*ejercicio 4.1 B)  */
+
+            double temperatura = 100;
+
+            if (temperatura > 100) { System.out.println("por encima del punto de ebullición del agua");
+                
+            }
+            else { System.out.println("por debajo del punto de ebullición del agua");}
 
 
-    }
+        /*ejercicio 4.1 C)  */
+
+            int positivos = 0; int negativos = 0; int numero = 10;
+
+             if (numero >= 0 ) {
+            int   TOTALPOSITIVOS =  positivos + numero;
+            System.out.println("total positivos: " + TOTALPOSITIVOS);
+        } else {
+            int TOTALNEGATIVOS = negativos + numero; 
+            System.out.println("Total negativos: " + TOTALNEGATIVOS);}
+        
+        
+         /*ejercicio 4.1 D)  */
+        
+         int x = 100; int y = 20; int z = 10;
+
+         if (x > y && z < 20) {
+         int P = x * y;
+         System.out.println("La distancia P es: "+ P);
+            
+         } else { System.out.println("no es posible calcular distancia para P ");}
+            
+          /*ejercicio 4.1 E)  */
+
+        int distancia = 21; 
+
+        if (distancia > 20 && distancia < 35 ) {
+        int tiempo = 100; 
+        System.out.println("el tiempo vale: " + tiempo + " segundos");}
+        
+        else { System.out.println("no es posible calcular el tiempo ");}
+            
+        
+         
 
     
+
     
+}  
 } 
 
     
