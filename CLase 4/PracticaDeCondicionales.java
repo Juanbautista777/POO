d18f5caf2c03ed309f669363178d549187318ab1
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-08-27 20:21:28 +0000

    primera prueba con un if

---
+++ CLase 4/PracticaDeCondicionales.java
@@ -0,0 +1,52 @@
+public class PracticaDeCondicionales {
+
+    public static void main (String[] args){
+        
+        int A = 100;     
+        
+
+        if (A == 0) { System.err.println("good ending");
+            
+
+
+
+
+
+
+
+        
+        }
+
+
+
+
+
+    }
+
+    
+    
+} 
+
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
