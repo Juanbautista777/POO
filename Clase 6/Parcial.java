e733ad542ec6d923d659f89bfe67ae9e71d82ffd
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-10-15 21:32:54 +0000

    se corrige la clase main (parcial)

--- Clase 6/Parcial.java
+++ Clase 6/Parcial.java
@@ -1,14 +1,15 @@
 public class Parcial {
-
     public static void main(String[] args) {
-        CuentaBancaria cuenta = new CuentaBancaria("Juan", "123456",100);
+
+        // --- Creacion de una cuenta bancaria ---
+        CuentaBancaria cuenta = new CuentaBancaria("Juan", "123456", 100);
 
         cuenta.depositar(50);
         cuenta.retirar(170);
         System.out.println(cuenta);
 
+        // --- Creacion de un producto ---
         Producto p1 = new Producto("Laptop", 2500.0, 2, 10);
         System.out.println(p1);
-
     }
 }
