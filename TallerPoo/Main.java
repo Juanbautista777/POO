963329e453fcea162ccbb1eeb73c03b7dff5c1ac
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-11-03 15:44:35 +0000

    buscando el error del code

--- TallerPoo/Main.java
+++ TallerPoo/Main.java
@@ -2,6 +2,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+
         Cliente cliente = new Cliente("123456", "Juan Bautista");
 
         Foto foto1 = new Foto("foto_vacaciones.png");
@@ -9,7 +10,7 @@ public class Main {
 
         Camara camara1 = new Camara(2, "Canon", "EOS 2000D");
 
-        Producto[] productos = {impresion1, camara1};
+        Producto[] productos = { impresion1, camara1 };
 
         Pedido pedido = new Pedido(cliente, productos, new Date(), 987654321);
         pedido.mostrarPedido();
