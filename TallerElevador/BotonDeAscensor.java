bcfcd3bff250119ca5bc4bffa74c3e1df2f0231b
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-11-06 20:52:04 +0000

    a aborts

---
+++ TallerElevador/BotonDeAscensor.java
@@ -0,0 +1,14 @@
+public class BotonDeAscensor extends boton {
+    private int pisoDestino;
+
+    public BotonDeAscensor(int pisoDestino) {
+        super();
+        this.pisoDestino = pisoDestino;
+    }
+
+    public int getPisoDestino() {
+        return pisoDestino;
+    }
+}
+    
+
---
+++ TallerElevador/BotonDePiso.java
@@ -0,0 +1,12 @@
+public class BotonDePiso {
+    private String direccion; // bajar o subir
+
+    public BotonDePiso(String direccion) {
+        super();
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+}
