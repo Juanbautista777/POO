a0b11ae24026cac2b5b45b15b230a190327ba278
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-11-06 23:20:50 +0000

    se crea la clase piso y se corrige boton de piso

--- TallerElevador/BotonDePiso.java
+++ TallerElevador/BotonDePiso.java
@@ -1,6 +1,5 @@
-public class BotonDePiso {
-    private String direccion; // bajar o subir
-
+public class BotonDePiso extends boton {
+    private String direccion; 
     public BotonDePiso(String direccion) {
         super();
         this.direccion = direccion;
