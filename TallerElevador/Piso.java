46bc68c5d9180dbfa2a97b619d2e1aecf4b5c8af
Juanbautista777 <juan.bautistaenc@campusucc.edu.co>
2025-11-18 15:35:30 +0000

    se corrige/mejora la clase piso

--- TallerElevador/Piso.java
+++ TallerElevador/Piso.java
@@ -5,21 +5,34 @@ public class Piso {
 
     public Piso(int numero, boolean primerPiso, boolean ultimoPiso) {
         this.numero = numero;
-        if (!primerPiso)
-            botonBajar = new BotonDePiso("bajar");
-        if (!ultimoPiso)
-            botonSubir = new BotonDePiso("subir");
+        if (!primerPiso) {
+            botonBajar = new BotonDePiso(Direccion.BAJAR);
+        }
+        if (!ultimoPiso) {
+            botonSubir = new BotonDePiso(Direccion.SUBIR);
+        }
     }
 
-    public int getNumero() {
-        return numero;
-    }
+    public int getNumero() { return numero; }
 
     public void presionarSubir() {
-        if (botonSubir != null) botonSubir.presionar();
+        if (botonSubir != null) {
+            System.out.println("🔼 Piso " + numero + " solicita SUBIR");
+            botonSubir.presionar();
+        } else {
+            System.out.println("❌ No hay botón de subir en el último piso");
+        }
     }
 
     public void presionarBajar() {
-        if (botonBajar != null) botonBajar.presionar();
+        if (botonBajar != null) {
+            System.out.println("🔽 Piso " + numero + " solicita BAJAR");
+            botonBajar.presionar();
+        } else {
+            System.out.println("❌ No hay botón de bajar en el primer piso");
+        }
     }
+
+    public boolean tieneBotonSubir() { return botonSubir != null; }
+    public boolean tieneBotonBajar() { return botonBajar != null; }
 }
